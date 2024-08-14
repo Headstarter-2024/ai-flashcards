ee142083a9655cd91c9e8944743591ca6eecfdd1
Anuj Patel <anujpatel1326@gmail.com>
2024-08-14 19:26:09 -0400

    Added Final Basic Features

--- app/page.js
+++ app/page.js
@@ -1,3 +1,5 @@
+"use client";
+
 import Image from "next/image";
 import getStripe from "@/utils/get-stripe";
 import {
@@ -13,6 +15,31 @@ import Head from "next/head";
 import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
+  const handleSubmit = async () => {
+    const checkoutSession = await fetch("/api/checkout_session", {
+      method: "POST",
+      headers: {
+        origin: "http://localhost:3000 ",
+      },
+    });
+
+    const checkoutSessionJson = await checkoutSession.json();
+
+    if (checkoutSession.statusCode === 500) {
+      console.error(checkoutSession.message);
+      return;
+    }
+
+    const stripe = await getStripe();
+    const { error } = await stripe.redirectToCheckout({
+      sessionId: checkoutSessionJson.id,
+    });
+
+    if (error) {
+      console.warn(error.message);
+    }
+  };
+
   return (
     <Container maxWidth={false} disableGutters style={{ width: "100%" }}>
       <Head>
@@ -135,7 +162,12 @@ export default function Home() {
                 Unlimited flashcard features and storage, with priority support
                 included!
               </Typography>
-              <Button variant="contained" color="primary" sx={{ mt: 2 }}>
+              <Button
+                variant="contained"
+                color="primary"
+                sx={{ mt: 2 }}
+                onClick={handleSubmit}
+              >
                 Choose Pro
               </Button>
             </Box>
