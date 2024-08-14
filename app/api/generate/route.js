b5f7374999e3237e3a88da7f06c4633ed7234ccb
Anuj Patel <anujpatel1326@gmail.com>
2024-08-14 16:41:58 -0400

    Added the Ability to Generate Flashcards

--- app/api/generate/route.js
+++ app/api/generate/route.js
@@ -3,9 +3,7 @@ import { NextResponse } from "next/server";
 import OpenAI from "openai";
 
 const systemPrompt = `
-
-You are a flashcard creator. Your task is to generate concise and effective flashcards based on the given topic or content. Follow these
-guidelines:
+You are a flashcard creator. Your task is to generate concise and effective flashcards based on the given topic or content. Follow these guidelines:
 
 1. Create clear and concise questions for the front of the flashcard.
 2. Provide accurate and informative answers for the back of the flashcard.
@@ -16,7 +14,9 @@ guidelines:
 7. When appropriate, use mnemonics or memory aids to help reinforce the information.
 8. Tailor the difficulty level of the flashcards to the user's specified preferences.
 9. If given a body of text, extract the most important and relevant information for the flashcards.
-10. Aim to create a balanced set of flashcards that covers the topic comprehensively.
+10. Ensure that the back of the flashcard provides the answer directly without repeating the question or using redundant phrases.
+11. Aim to create a balanced set of flashcards that covers the topic comprehensively.
+12. Only generate 10 flashcards.
 
 Remember, the goal is to facilitate effective learning and retention of information through these flashcards.
 
@@ -32,14 +32,14 @@ Return in the following JSON format
 `;
 
 export async function POST(req) {
-  const openai = OpenAI();
+  const openai = new OpenAI();
   const data = await req.text();
-  const completion = await openai.chat.completion.create({
+  const completion = await openai.chat.completions.create({
     messages: [
       { role: "system", content: systemPrompt },
       { role: "user", content: data },
     ],
-    model: "gpt-4o",
+    model: "gpt-4o-mini",
     response_format: { type: "json_object" },
   });
   const flashcards = JSON.parse(completion.choices[0].message.content);
