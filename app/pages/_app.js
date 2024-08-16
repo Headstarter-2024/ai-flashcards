94f27f73ec288d27edffad2e00072e1bfe59d5d8
Anuj Patel <anujpatel1326@gmail.com>
2024-08-16 17:28:08 -0400

    Added Google Analytics

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import '../styles/globals.css';
+import React, { useEffect } from "react";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import Script from "next/script";
+import "../styles/globals.css";
 
 // Create a theme instance.
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#1976d2',
+      main: "#1976d2",
     },
     background: {
-      default: '#e0f7fa',
+      default: "#e0f7fa",
     },
   },
   typography: {
@@ -24,9 +25,37 @@ const theme = createTheme({
 });
 
 export default function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    if (process.env.NODE_ENV === "production") {
+      window.dataLayer = window.dataLayer || [];
+      function gtag() {
+        window.dataLayer.push(arguments);
+      }
+      gtag("js", new Date());
+      gtag("config", "G-T84G17G733");
+    }
+  }, []);
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
+      {/* Add Google Analytics Script */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS},');
+          `,
+        }}
+      />
       <Component {...pageProps} />
     </ThemeProvider>
   );
